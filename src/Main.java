c61a8111449414e0dcc0f757595ca5c8154d9cba
vshvidchenko <vshvidchenko@mail.ru>
2023-02-15 10:36:10 +0300

    task4-5

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
     }
     public static void task1 () {
         var dog = 8.0;
@@ -33,7 +35,22 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-
+            }
+    public static void task4 () {
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+    public static void task5 () {
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10 / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
 
